84e3fabbf6cdf7f6f1653f4ed36d03fa424316a2
Blake Sykes <blakesykes13@gmail.com>
2019-09-18 23:12:23 -0600

    added functions to write the question and answers to the html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,10 +5,10 @@ console.log("JS READY")
 var questions = [
     q0 = {
         "text": "question 0",
-        "a1": "answer 1",
-        "a2": "answer 2",
-        "a3": "answer 3",
-        "correctA": "a1"
+        "a": [  "answer 0",
+                "answer 1",
+                "answer 2"],
+        "correctA": "0"
     }
 ]
 
@@ -16,9 +16,25 @@ var currentQuestion = 0;
 var correct = 0;
 var incorrect = 0;
 var timeOut = 0;
+var selectedAnswer;
 
-console.log(questions[0].text)
+console.log(questions[0].a[0]);
 
-console.log(currentQuestion)
+console.log(currentQuestion);
 
+function writeQuestion(){
+    $("#q").text(questions[currentQuestion].text);
+};
+
+function writeAnswers(x){
+    var answerDiv = $("<div>");
+    answerDiv.attr("id",x);
+    answerDiv.attr("selected",false);
+    answerDiv.attr("class","answerDiv");
+    answerDiv.text(questions[currentQuestion].a[x]);
+    $("#a").append(questions[currentQuestion].a[x]);
+};
+
+writeQuestion();
+writeAnswers(0);
     

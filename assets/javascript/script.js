988fa288d00464315624ca1abb35047f639ccbff
Blake Sykes <blakesykes13@gmail.com>
2019-09-18 22:53:47 -0600

    created questions array with one question object inside

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+
+
+console.log("JS READY")
+
+var questions = [
+    q0 = {
+        "text": "question 0",
+        "a1": "answer 1",
+        "a2": "answer 2",
+        "a3": "answer 3",
+        "correctA": "a1"
+    }
+]
+
+var currentQuestion = 0;
+var correct = 0;
+var incorrect = 0;
+var timeOut = 0;
+
+console.log(questions[0].text)
+
+console.log(currentQuestion)
+
+    
